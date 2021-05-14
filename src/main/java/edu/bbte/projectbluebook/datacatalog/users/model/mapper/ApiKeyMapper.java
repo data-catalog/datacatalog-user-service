ebdc829dd0b6b9ebaa06bde5a3de9f9d09ab625a
Tumó Szilárd <tumoszilard@gmail.com>
2021-05-14 19:16:46 +0300

    Fixed createdAt field not being set.

--- src/main/java/edu/bbte/projectbluebook/datacatalog/users/model/mapper/ApiKeyMapper.java
+++ src/main/java/edu/bbte/projectbluebook/datacatalog/users/model/mapper/ApiKeyMapper.java
@@ -5,11 +5,13 @@ import edu.bbte.projectbluebook.datacatalog.users.model.dto.ApiKeyCreationReques
 import edu.bbte.projectbluebook.datacatalog.users.model.dto.ApiKeyCreationResponse;
 import edu.bbte.projectbluebook.datacatalog.users.model.dto.ApiKeyResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.time.OffsetDateTime;
 
 @Mapper(componentModel = "spring", imports = {OffsetDateTime.class})
 public abstract class ApiKeyMapper {
+    @Mapping(target = "createdAt", expression = "java(OffsetDateTime.now())")
     public abstract ApiKey requestDtoToModel(ApiKeyCreationRequest apiKeyCreationRequest);
 
     public abstract ApiKeyCreationResponse modelToCreationResponseDto(ApiKey apiKey);
